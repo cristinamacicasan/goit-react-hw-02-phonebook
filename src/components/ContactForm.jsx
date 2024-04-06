3b0d8c1984d33af916135d3418f04ba2951c31de
cristinamacicasan <cmacicasan@gmail.com>
2024-04-06 22:55:22 +0300

    update Contactform.jsx cu step 2

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,13 +4,15 @@ import React, { useState } from 'react';
 import styles from './ContactForm.module.css'
 
 const ContactForm = ({ onAddContact }) => {
-  const [name, setName] = useState('');
+    const [name, setName] = useState('');
+     const [number, setNumber] = useState('');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!name.trim()) return;
-    onAddContact(name);
+    if (!name.trim() || !number.trim()) return;
+    onAddContact({ name, number });
     setName('');
+    setNumber('');
   };
 
   return (
@@ -21,6 +23,16 @@ const ContactForm = ({ onAddContact }) => {
         id="name"
         value={name}
         onChange={(e) => setName(e.target.value)}
+        required
+        />
+        <label htmlFor="number">Number:</label>
+        <input
+        type="tel"
+        id="number"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
       <button type="submit">Add Contact</button>
